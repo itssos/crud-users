b928ee9878eeb75e9cfd65f6db9e10da73f293e2
Sair Marquez <sair2003.hotmail.com@gmail.com>
2024-06-02 18:28:11 -0500

    change: viewModel.php

--- app/models/viewsModel.php
+++ app/models/viewsModel.php
@@ -3,5 +3,21 @@
 namespace app\models;
 
 class viewsModel {
-    
+
+    protected function getModelViews($view){
+        $whiteList = ["dashboard"];
+
+        if(in_array($view, $whiteList)){
+            if(is_file("./app/views/content/".$view."-view.php")){
+                $content = "./app/views/content/".$view."-view.php";
+            }else{
+                $content = "404";
+            }
+        }elseif($view == "login" || $view == "index"){
+            $content = "login";
+        }else{
+            $content = "404";
+        }
+        return $content;
+    }
 }
