0844b48024307253d80085111d796e6654bb0009
Sair Marquez <sair2003.hotmail.com@gmail.com>
2024-06-02 18:31:12 -0500

    change: viewController.php

--- app/controllers/viewsController.php
+++ app/controllers/viewsController.php
@@ -4,5 +4,12 @@ namespace app\controllers;
 use app\models\viewsModel;
 
 class viewsController extends viewsModel{
-    
+    public function getControllerViews($view){
+        if($view != ""){
+            $response = $this->getModelViews($view);
+        }else{
+            $response = "login";
+        }
+        return $response;
+    }
 }
